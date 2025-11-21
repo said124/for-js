953cb6894393037eaff75cecdf181b1929b5c829
said124 <165512725+said124@users.noreply.github.com>
2025-11-21 10:00:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// // // // 1 // // // //
+
+// let numbers = prompt('Введите число')
+// if (numbers > 50) {
+//     alert(`Correct: ${numbers}`)
+// } else{
+//     alert(`Uncorrect: ${numbers}`)
+// }
+
+// // // // 2 // // // //
+
+// let numbers = [10, 65, 30, 80, 5, 99];
+
+// for (let i = 0;  i < numbers.length; numbers++) {
+//     console.log(numbers);
+    
+    
+// }
+
+// // // // 3 // // // //
+
+// let age = prompt('Укажите возвраст')
+
+// if (age>18) {
+//     alert("Взрослый")
+// } 
+// else if (age>60) {
+//     alert("Пожилой человек");
+    
+// } else {
+//     alert("Несовершеннолетний")
+// }
+
